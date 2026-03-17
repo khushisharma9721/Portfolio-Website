be2d44585d40c152db2aaaf8e112d438ae26bf15
Khushi sharma <khushisharma9721@gmail.com>
2026-03-17 15:07:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// Typing animation
+
+let text = ["Frontend Developer","Web Developer"]
+
+let count = 0
+
+let index = 0
+
+let currentText = ""
+
+let letter = ""
+
+function type(){
+
+if(count === text.length){
+
+count = 0
+
+}
+
+currentText = text[count]
+
+letter = currentText.slice(0,++index)
+
+document.getElementById("typing").textContent = letter
+
+if(letter.length === currentText.length){
+
+count++
+
+index = 0
+
+}
+
+setTimeout(type,150)
+
+}
+
+type()
+
+
+// Dark mode
+
+const btn = document.getElementById("mode");
+
+btn.onclick = function(){
+document.body.classList.toggle("dark");
+}
